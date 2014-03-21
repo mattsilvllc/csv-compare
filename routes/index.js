4e12316d567f3fcd26c3a66a4f106968c1d95e9e
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2014-03-22 02:13:10 +0400

    changes in result

--- routes/index.js
+++ routes/index.js
@@ -26,20 +26,21 @@ exports.compare = function (req, res) {
       _.each(row, function (col, col_ind) {
         if (col !== file2[row_ind][col_ind]) {
           no_match = true;
+          row[col_ind] = '';
           return;
         }
       });
 
-      if (no_match) result.push(row[0]);
+      if (no_match) {
+        result.push(row);
+      }
     });
 
     if (result.length === 0) {
       res.send('no mismatch');
     } else {
-      result.unshift('id');
-      result = result.map(function (d) { return [d]; });
+      result.unshift(header1);
       result = csv.stringify(result);
-
       res.set('Content-disposition', 'filename=' + (new Date ()).getTime() + '.csv');
       res.set('Content-Type', 'application/octet-stream');
       res.send(result);
